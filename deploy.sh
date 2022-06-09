44dcbe7959f81d3985acf7715addd6ee7d68cf8a
Yurii Timofeev <georgius@ural.ru>
2022-06-09 23:54:14 +0500

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -19,5 +19,5 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:georgius1024/heatmap-24-7.git main:gh-pages
-
+echo https://georgius1024.github.io/heatmap-24-7
 cd -
